50490262c2dc46c17bd6b1fa8c8d324bdb55f844
schaffsp <schaffsp@dukes.jmu.edu>
2021-12-27 21:18:04 -0500

    CSS upgrades

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,6 +5,8 @@ const Navbar = () => {
     return (
         <div className='Navbar-Container'>
             <h1 className='Navbar-Component'>Drivethrough Rework</h1>
+            <button className='Navbar-Component Navbar-Button'>How does it work?</button>
+            <button className='Navbar-Component Navbar-Button'>About</button>
         </div>
     )
 }
--- src/Components/Restaurant.js
+++ src/Components/Restaurant.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import '../CSS/Restaurant.css'
 
 const Restaurant = (props) => {
     const {restaurant} = props;
 
+    /*
+    / The restaurant object returned by the Google Places API has the following useful properties:
+    /
+    / business_status
+    / geometry
+    /   --> location {lat: X, lng: X}
+    /   --> name
+    / opening_hours
+    /   --> open_now: [true / false]
+    / place_id
+    / price_level: [integer]
+    / types: [Array]
+    / vicinity: [Address as a String]
+    */
+
     return (
-        <div>
-            <h2>{restaurant.name}</h2>
+        <div className='Restaurant-Contianer'>
+            <h2>{restaurant.name} . . . . <span className='Restaurant-OpenNow'>Test</span></h2>
         </div>
     )
 }
